637c662e38d2a7b82a5c18ac4385c07612822744
hunterhector <hunterhector@gmail.com>
2014-10-07 14:13:23 -0400

    Add a logging annotator

---
+++ uima-util/src/main/java/edu/cmu/cs/lti/uima/annotator/AbstractLoggingAnnotator.java
@@ -0,0 +1,46 @@
+package edu.cmu.cs.lti.uima.annotator;
+
+import edu.cmu.cs.lti.uima.util.UimaConvenience;
+import org.apache.uima.UimaContext;
+import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhengzhongliu
+ * Date: 10/7/14
+ * Time: 1:45 PM
+ */
+public abstract class AbstractLoggingAnnotator extends JCasAnnotator_ImplBase{
+    public static final String PARAM_KEEP_QUIET = "kee_quiet";
+
+    private String className = this.getClass().getName();
+
+    protected Logger logger = Logger.getLogger(className);
+
+    @ConfigurationParameter(name = PARAM_KEEP_QUIET, mandatory = false)
+    private Boolean keepQuiet;
+
+    @Override
+    public void initialize(UimaContext aContext) throws ResourceInitializationException {
+        super.initialize(aContext);
+        keepQuiet = (Boolean) aContext.getConfigParameterValue(PARAM_KEEP_QUIET);
+        //default is quiet
+        keepQuiet = keepQuiet == null ? true : keepQuiet;
+
+        if (keepQuiet) {
+            logger.setLevel(Level.SEVERE);
+        }else{
+            logger.setLevel(Level.INFO);
+        }
+    }
+
+    protected void logProgress(JCas aJCas){
+        logger.log(Level.INFO, "Processing " + UimaConvenience.getShortDocumentNameWithOffset(aJCas));
+    }
+}
