138fe9a3093f63a851f2e977f760659aa3b8dbba
hunterhector <hunterhector@gmail.com>
2015-04-21 01:01:41 -0400

    Update NoiseTextFormatter to let it handle more tags.

--- corpus-reader/src/main/java/edu/cmu/cs/lti/collection_reader/BratEventGoldStandardAnnotator.java
+++ corpus-reader/src/main/java/edu/cmu/cs/lti/collection_reader/BratEventGoldStandardAnnotator.java
@@ -6,7 +6,8 @@ import edu.cmu.cs.lti.model.Span;
 import edu.cmu.cs.lti.script.type.Event;
 import edu.cmu.cs.lti.script.type.EventMention;
 import edu.cmu.cs.lti.script.type.EventRelation;
-import edu.cmu.cs.lti.uima.annotator.AbstractCollectionReader;
+import edu.cmu.cs.lti.uima.annotator.AbstractAnnotator;
+import edu.cmu.cs.lti.uima.io.reader.PlainTextCollectionReader;
 import edu.cmu.cs.lti.uima.io.writer.CustomAnalysisEngineFactory;
 import edu.cmu.cs.lti.uima.util.Comparators;
 import edu.cmu.cs.lti.uima.util.UimaAnnotationUtils;
@@ -16,10 +17,11 @@ import org.apache.commons.io.FileUtils;
 import org.apache.uima.UIMAException;
 import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.cas.CASException;
-import org.apache.uima.collection.CollectionException;
 import org.apache.uima.collection.CollectionReaderDescription;
 import org.apache.uima.fit.descriptor.ConfigurationParameter;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.factory.CollectionReaderFactory;
 import org.apache.uima.fit.factory.TypeSystemDescriptionFactory;
 import org.apache.uima.fit.pipeline.SimplePipeline;
@@ -29,8 +31,6 @@ import org.apache.uima.jcas.cas.FSArray;
 import org.apache.uima.jcas.tcas.Annotation;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.resource.metadata.TypeSystemDescription;
-import org.apache.uima.util.Progress;
-import org.apache.uima.util.ProgressImpl;
 import org.javatuples.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -46,16 +46,12 @@ import java.util.*;
  *
  * @author Zhengzhong Liu
  */
-public class BratEventAnnotationDataReader extends AbstractCollectionReader {
-
+public class BratEventGoldStandardAnnotator extends AbstractAnnotator {
     public static final String PARAM_SOURCE_TEXT_DIR = "sourceTextDir";
     public static final String PARAM_ANNOTATION_DIR = "annDir";
 
     public static final String PARAM_TEXT_FILE_NAME_SUFFIX = "textFileNameSuffix";
     public static final String PARAM_ANNOTATION_FILE_NAME_SUFFIX = "annotationFileNameSuffix";
-
-    @ConfigurationParameter(name = PARAM_SOURCE_TEXT_DIR)
-    private File sourceTextDir;
 
     @ConfigurationParameter(name = PARAM_ANNOTATION_DIR)
     private File annotationDir;
@@ -65,23 +61,22 @@ public class BratEventAnnotationDataReader extends AbstractCollectionReader {
 
     public static final String defaultTextFileNameSuffix = ".tkn.txt";
 
+
     @ConfigurationParameter(name = PARAM_ANNOTATION_FILE_NAME_SUFFIX, mandatory = false)
     private String annotationFileNameSuffix;
 
     public static final String defaultAnnotationFileNameSuffix = ".tkn.ann";
-
-    private Iterator<Pair<File, File>> iter;
-    private int processedDocs = 0;
-    private int totalDocs = 0;
 
     private static final String realisTypeName = "Realis";
     private static final String coreferenceLinkName = "Coreference";
     private static final String afterLinkName = "After";
     private static final String subeventLinkName = "Subevent";
 
-    private static final Logger logger = LoggerFactory.getLogger(BratEventAnnotationDataReader.class);
+    private static final Logger logger = LoggerFactory.getLogger(BratEventGoldStandardAnnotator.class);
 
-    public static final String COMPONENT_ID = BratEventAnnotationDataReader.class.getSimpleName();
+    public static final String COMPONENT_ID = BratEventGoldStandardAnnotator.class.getSimpleName();
+
+    private Map<String, File> annotationsByName;
 
     @Override
     public void initialize(UimaContext context) throws ResourceInitializationException {
@@ -93,62 +88,22 @@ public class BratEventAnnotationDataReader extends AbstractCollectionReader {
             annotationFileNameSuffix = defaultAnnotationFileNameSuffix;
         }
 
-
-        if (!sourceTextDir.isDirectory()) {
-            throw new IllegalArgumentException("Cannot find source directory " + sourceTextDir.getAbsolutePath());
-        }
-
         if (!annotationDir.isDirectory()) {
-            throw new IllegalArgumentException("Cannot find annotation direcotyr " + annotationDir.getAbsolutePath());
+            throw new IllegalArgumentException("Cannot find annotation directory " + annotationDir.getAbsolutePath());
         }
 
-        File[] sourceDocuments = edu.cmu.cs.lti.utils.FileUtils.getFilesWithSuffix(sourceTextDir, textFileNameSuffix);
         File[] annotationDocuments = edu.cmu.cs.lti.utils.FileUtils.getFilesWithSuffix(annotationDir, annotationFileNameSuffix);
 
-        List<Pair<File, File>> allSourceAndAnnotations = findCommonFiles(sourceDocuments, annotationDocuments);
-
-        iter = allSourceAndAnnotations.iterator();
-        processedDocs = 0;
-        totalDocs = allSourceAndAnnotations.size();
+        annotationsByName = findAnnotationFiles(annotationDocuments);
     }
 
-
-    private List<Pair<File, File>> findCommonFiles(File[] sourceDocuments, File[] annotationDocuments) {
-        Map<String, File> sourceDocumentByName = new HashMap<>();
-        for (File sourceDocument : sourceDocuments) {
-            sourceDocumentByName.put(StringUtils.removeEnd(sourceDocument.getName(), textFileNameSuffix), sourceDocument);
-        }
-
-        List<Pair<File, File>> sourceAndAnnotations = new ArrayList<>();
-
+    private Map<String, File> findAnnotationFiles(File[] annotationDocuments) {
+        Map<String, File> annotationDocByName = new HashMap<>();
         for (File annotationDocument : annotationDocuments) {
             String annotationDocName = StringUtils.removeEnd(annotationDocument.getName(), annotationFileNameSuffix);
-            if (sourceDocumentByName.containsKey(annotationDocName)) {
-                sourceDocumentByName.get(annotationDocName);
-                sourceAndAnnotations.add(Pair.with(sourceDocumentByName.get(annotationDocName), annotationDocument));
-            }
+            annotationDocByName.put(annotationDocName, annotationDocument);
         }
-
-        return sourceAndAnnotations;
-    }
-
-    @Override
-    public void getNext(JCas jCas) throws IOException, CollectionException {
-        Pair<File, File> sourceAndAnnotation = iter.next();
-        File sourceDocument = sourceAndAnnotation.getValue0();
-        File annotationDocument = sourceAndAnnotation.getValue1();
-        String sourceText = FileUtils.readFileToString(sourceDocument, encoding);
-        jCas.setDocumentText(FileUtils.readFileToString(sourceDocument, encoding));
-        JCas goldView = null;
-        try {
-            goldView = jCas.createView(goldStandardViewName);
-        } catch (CASException e) {
-            throw new RuntimeException(e);
-        }
-        goldView.setDocumentText(sourceText);
-
-        annotateGoldStandard(goldView, FileUtils.readLines(annotationDocument, encoding));
-        UimaAnnotationUtils.setSourceDocumentInformation(jCas, sourceDocument.toURI().toURL().toString(), 0, 0, true);
+        return annotationDocByName;
     }
 
     public void annotateGoldStandard(JCas aJCas, List<String> bratAnnotations) {
@@ -277,6 +232,25 @@ public class BratEventAnnotationDataReader extends AbstractCollectionReader {
         }
     }
 
+    @Override
+    public void process(JCas aJCas) throws AnalysisEngineProcessException {
+        JCas goldView = null;
+        try {
+            goldView = aJCas.createView(goldStandardViewName);
+        } catch (CASException e) {
+            throw new RuntimeException(e);
+        }
+        goldView.setDocumentText(aJCas.getDocumentText());
+        
+        File annotationDocument = annotationsByName.get(StringUtils.removeEnd(UimaConvenience.getDocId(aJCas), textFileNameSuffix));
+
+        try {
+            annotateGoldStandard(goldView, FileUtils.readLines(annotationDocument, encoding));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     public class Attribute {
         String attributeId;
@@ -340,18 +314,8 @@ public class BratEventAnnotationDataReader extends AbstractCollectionReader {
         return Pair.with(spans, type);
     }
 
-    @Override
-    public boolean hasNext() throws IOException, CollectionException {
-        return iter.hasNext();
-    }
 
-    @Override
-    public Progress[] getProgress() {
-        return new Progress[]{new ProgressImpl(processedDocs, totalDocs, Progress.ENTITIES)};
-    }
-
-
-    public static void main(String[] args) throws UIMAException {
+    public static void main(String[] args) throws UIMAException, IOException {
         logger.info(COMPONENT_ID + " started");
         String parentDir = "data/brat_event";
         String inputDir = parentDir + "/" + "LDC2014E121";
@@ -359,28 +323,24 @@ public class BratEventAnnotationDataReader extends AbstractCollectionReader {
 
         String paramTypeSystemDescriptor = "TypeSystem";
 
-        TypeSystemDescription typeSystemDescription = TypeSystemDescriptionFactory
-                .createTypeSystemDescription(paramTypeSystemDescriptor);
+        TypeSystemDescription typeSystemDescription = TypeSystemDescriptionFactory.createTypeSystemDescription(paramTypeSystemDescriptor);
 
         CollectionReaderDescription reader = CollectionReaderFactory.createReaderDescription(
-                BratEventAnnotationDataReader.class, typeSystemDescription,
-                BratEventAnnotationDataReader.PARAM_ANNOTATION_DIR, inputDir,
-                BratEventAnnotationDataReader.PARAM_SOURCE_TEXT_DIR, inputDir
+                PlainTextCollectionReader.class,
+                PlainTextCollectionReader.PARAM_INPUTDIR, inputDir,
+                PlainTextCollectionReader.PARAM_TEXT_SUFFIX, ".txt");
+
+        AnalysisEngineDescription engine = AnalysisEngineFactory.createEngineDescription(
+                BratEventGoldStandardAnnotator.class, typeSystemDescription,
+                BratEventGoldStandardAnnotator.PARAM_ANNOTATION_DIR, inputDir,
+                BratEventGoldStandardAnnotator.PARAM_SOURCE_TEXT_DIR, inputDir
         );
 
         AnalysisEngineDescription writer = CustomAnalysisEngineFactory.createXmiWriter(
                 parentDir, baseDir, null, null
         );
 
-
-        // Run the pipeline.
-        try {
-            SimplePipeline.runPipeline(reader, writer);
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-
+        SimplePipeline.runPipeline(reader, engine, writer);
         System.out.println(COMPONENT_ID + " successfully completed.");
     }
 }
--- corpus-reader/src/main/java/edu/cmu/cs/lti/util/NoiseTextFormatter.java
+++ corpus-reader/src/main/java/edu/cmu/cs/lti/util/NoiseTextFormatter.java
@@ -5,12 +5,21 @@ import de.l3s.boilerpipe.BoilerpipeProcessingException;
 import de.l3s.boilerpipe.extractors.ArticleExtractor;
 import de.l3s.boilerpipe.extractors.ExtractorBase;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.sax.BodyContentHandler;
 import org.xml.sax.SAXException;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created with IntelliJ IDEA.
@@ -21,54 +30,72 @@ import java.util.List;
 public class NoiseTextFormatter {
     private static ExtractorBase extractor = ArticleExtractor.getInstance();
 
-    private static String[] forumPatterns = {"<post", "<quote", "< / quote", "< / post>"};
+    private static String[] forumPatterns = {"<post([^<].*?)>", "<quote[^<].*?>", "< / quote>", "< / post>", "<img[^<].*>", "<a href=.*?>"};
 
-    public static String articleExtract(String text) throws BoilerpipeProcessingException, SAXException {
+    public static String extractArticle(String text) throws BoilerpipeProcessingException, SAXException {
         return extractor.getText(text);
     }
 
     public static String cleanForum(String docText) {
-        String[] lines = docText.split("\n");
+        //apply the newline even as many times as possible to make it identical to input
+        String[] lines = docText.split("\n", -1);
+
         List<String> cleanedLines = new ArrayList<>();
         for (String line : lines) {
-            cleanedLines.add(replaceMathedTokenWithChar(line, forumPatterns, '-'));
+            cleanedLines.add(replaceMatchedWithChar(line, forumPatterns, ' '));
         }
+
         return Joiner.on("\n").join(cleanedLines);
     }
 
+    public static String cleanTag(String docText, String encoding) throws IOException, TikaException, SAXException {
+        InputStream stream = IOUtils.toInputStream(docText);
+        AutoDetectParser parser = new AutoDetectParser();
+        BodyContentHandler handler = new BodyContentHandler();
+        Metadata metadata = new Metadata();
+        try {
+            parser.parse(stream, handler, metadata);
+            return handler.toString();
+        } finally {
+            stream.close();
+        }
+    }
 
-    public static String replaceMathedTokenWithChar(String line, String[] patterns, char c) {
-        for (String pattern : patterns) {
-            if (line.startsWith(pattern)) {
-                char[] replaced = new char[line.length()];
-                int index = 0;
-                for (char origin : line.toCharArray()) {
-                    if (origin == ' ') {
-                        replaced[index] = origin;
-                    } else {
-                        replaced[index] = c;
-                    }
-                    index++;
-                }
-                for (int i = replaced.length - 2; i > 0; i--) {
-                    if (replaced[i] == ' ') {
-                        break;
-                    }
-                    replaced[i] = '!';
-                }
-                return String.valueOf(replaced);
-            }
+    public static String replaceMatchedWithChar(String line, String[] patterns, char c) {
+        if (line.isEmpty()) {
+            return line;
         }
 
+        for (String pattern : patterns) {
+            Pattern p = Pattern.compile("("+pattern+")");
+            Matcher m = p.matcher(line);
+            StringBuffer sb = new StringBuffer();
+
+            while (m.find()) {
+                String found = m.group(1);
+                String replacementStr = StringUtils.repeat(c, found.length());
+                m.appendReplacement(sb, replacementStr);
+            }
+            m.appendTail(sb);
+            if (sb.length() != 0) {
+                line = sb.toString();
+            }
+        }
         return line;
     }
 
-    public static void main(String[] args) throws BoilerpipeProcessingException, IOException, SAXException {
-        System.out.println(
-                articleExtract(FileUtils.readFileToString(
-                        new File("/Users/zhengzhongliu/Documents/projects/uimafied-tools/data/event_mention_detection/" +
-                                "LDC2014E121_DEFT_Event_Nugget_Evaluation_Training_Data/data/source/" +
-                                "2f5ee4e363c30678dc3b55caf43bc63d.cmp.tkn.txt"))));
-    }
+    public static void main(String[] args) throws BoilerpipeProcessingException, IOException, SAXException, TikaException {
+        String noisyText = FileUtils.readFileToString(
+                new File("/Users/zhengzhongliu/Documents/projects/uima-base-tools/data/event_mention_detection/" +
+                        "LDC2014E121_DEFT_Event_Nugget_Evaluation_Training_Data/data/source/" +
+                        "2f5ee4e363c30678dc3b55caf43bc63d.cmp.tkn.txt"));
 
+        System.out.println("=== Rule results ===");
+        String ruleCleaned = cleanForum(noisyText);
+//        System.out.println(ruleCleaned);
+
+
+        System.out.println("Original length " + noisyText.length());
+        System.out.println("Rule length " + ruleCleaned.length());
+    }
 }
