bd8f1c3871c9368753479b113f9f2480581813f1
hunterhector <hunterhector@gmail.com>
2015-09-02 15:38:55 -0400

    Rename a type.

--- uima-util/src/main/java/edu/cmu/cs/lti/uima/annotator/AbstractAnnotator.java
+++ uima-util/src/main/java/edu/cmu/cs/lti/uima/annotator/AbstractAnnotator.java
@@ -1,9 +1,8 @@
 package edu.cmu.cs.lti.uima.annotator;
 
-import org.apache.uima.UimaContext;
+import edu.cmu.cs.lti.model.UimaConst;
 import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
 import org.apache.uima.fit.descriptor.ConfigurationParameter;
-import org.apache.uima.resource.ResourceInitializationException;
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,28 +16,15 @@ public abstract class AbstractAnnotator extends JCasAnnotator_ImplBase {
 
     public final static String PARAM_ENCODING = "encoding";
 
-    @ConfigurationParameter(mandatory = false, description = "The view name for the golden standard view", name =
-            PARAM_GOLD_STANDARD_VIEW_NAME)
-    protected String goldStandardViewName;
-
-    @ConfigurationParameter(mandatory = false, description = "Specify the encoding of the input", name = PARAM_ENCODING)
-    protected String encoding;
-
     public static final String DEFAULT_ENCODING = "UTF-8";
 
-    public static final String DEFAULT_GOLD_STANDARD_NAME = "GoldStandard";
+    @ConfigurationParameter(description = "The view name for the golden standard view", name =
+            PARAM_GOLD_STANDARD_VIEW_NAME, defaultValue = UimaConst.goldViewName)
+    protected String goldStandardViewName;
+
+    @ConfigurationParameter(description = "Specify the encoding of the input", name = PARAM_ENCODING, defaultValue =
+            DEFAULT_ENCODING)
+    protected String encoding;
 
     public final String COMPONENT_ID = this.getClass().getSimpleName();
-
-    @Override
-    public void initialize(UimaContext context) throws ResourceInitializationException {
-        super.initialize(context);
-        if (goldStandardViewName == null) {
-            goldStandardViewName = DEFAULT_GOLD_STANDARD_NAME;
-        }
-        if (encoding == null) {
-            encoding = DEFAULT_ENCODING;
-        }
-    }
-
 }
