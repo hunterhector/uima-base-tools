5e551a77833454c7522e855806e0324b2587e6ec
hunterhector <hunterhector@gmail.com>
2019-06-16 11:55:56 -0400

    Debug allennlp arguments.

--- srl-parser/src/main/java/edu/cmu/cs/lti/annotators/AllenNLPJsonAnnotator.java
+++ srl-parser/src/main/java/edu/cmu/cs/lti/annotators/AllenNLPJsonAnnotator.java
@@ -13,21 +13,19 @@ import edu.cmu.cs.lti.uima.util.UimaNlpUtils;
 import org.apache.uima.UIMAException;
 import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.collection.CollectionReaderDescription;
-import org.apache.uima.collection.metadata.CpeDescriptorException;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.factory.TypeSystemDescriptionFactory;
 import org.apache.uima.fit.util.FSCollectionFactory;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.resource.metadata.TypeSystemDescription;
-import org.apache.uima.fit.descriptor.ConfigurationParameter;
-import org.xml.sax.SAXException;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -65,7 +63,7 @@ import java.util.Map;
  *
  * @author Zhengzhong Liu
  */
-public class JsonPropBankAnnotator extends AbstractLoggingAnnotator {
+public class AllenNLPJsonAnnotator extends AbstractLoggingAnnotator {
     public static final String PARAM_JSON_SRL_INPUT_DIR = "jsonSrlInput";
     @ConfigurationParameter(name = PARAM_JSON_SRL_INPUT_DIR)
     private String srlDataDir;
@@ -73,6 +71,8 @@ public class JsonPropBankAnnotator extends AbstractLoggingAnnotator {
     public static final String PARAM_INPUT_COMPONENT_NAME = "inputComponentName";
     @ConfigurationParameter(name = PARAM_INPUT_COMPONENT_NAME, mandatory = false)
     private String inputComponentName;
+
+    public static final String ALLENNLP_COMPONENT = "allennlp";
 
     private Gson gson;
 
@@ -86,6 +86,8 @@ public class JsonPropBankAnnotator extends AbstractLoggingAnnotator {
             List<Integer> span;
         }
     }
+
+    private  int count = 0;
 
     @Override
     public void initialize(UimaContext aContext) throws ResourceInitializationException {
@@ -117,11 +119,17 @@ public class JsonPropBankAnnotator extends AbstractLoggingAnnotator {
 
         try {
             FileReader reader = new FileReader(jsonFile);
+            logger.info("Reading from " + jsonFile.getAbsolutePath());
             SRLDoc doc = gson.fromJson(reader, SRLDoc.class);
             annotateSRL(aJCas, doc);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void collectionProcessComplete() throws AnalysisEngineProcessException {
+        super.collectionProcessComplete();
     }
 
     private void annotateSRL(JCas aJCas, SRLDoc doc) {
@@ -135,13 +143,14 @@ public class JsonPropBankAnnotator extends AbstractLoggingAnnotator {
                 } else {
                     argSpans.put(target.role, target.span);
                 }
+                count ++;
             }
 
             if (verbSpan != null) {
-                StanfordCorenlpToken srlHead = UimaConvenience.selectCoveredFirst(
+                StanfordCorenlpToken firstPredToken = UimaConvenience.selectCoveredFirst(
                         aJCas, verbSpan.get(0), verbSpan.get(1), StanfordCorenlpToken.class);
 
-                if (srlHead != null) {
+                if (firstPredToken != null) {
                     List<SemanticRelation> relations = new ArrayList<>();
                     for (Map.Entry<String, List<Integer>> argEntry : argSpans.entrySet()) {
                         String role = argEntry.getKey();
@@ -152,20 +161,20 @@ public class JsonPropBankAnnotator extends AbstractLoggingAnnotator {
 
                         SemanticRelation rel = new SemanticRelation(aJCas);
                         rel.setChild(argument);
-                        rel.setHead(srlHead);
+                        rel.setHead(firstPredToken);
                         rel.setPropbankRoleName(role);
                         UimaAnnotationUtils.finishTop(rel, inputComponentName, 0, aJCas);
 
                         relations.add(rel);
                     }
 
-                    srlHead.setChildSemanticRelations(FSCollectionFactory.createFSList(aJCas, relations));
+                    firstPredToken.setChildSemanticRelations(FSCollectionFactory.createFSList(aJCas, relations));
                 }
             }
         }
     }
 
-    public static void main(String[] args) throws UIMAException, IOException, CpeDescriptorException, SAXException {
+    public static void main(String[] args) throws UIMAException {
         TypeSystemDescription typeSystemDescription = TypeSystemDescriptionFactory
                 .createTypeSystemDescription("TypeSystem");
 
@@ -175,10 +184,11 @@ public class JsonPropBankAnnotator extends AbstractLoggingAnnotator {
 
         CollectionReaderDescription reader = CustomCollectionReaderFactory.createXmiReader(
                 typeSystemDescription, inputPath);
+
         AnalysisEngineDescription annotator = AnalysisEngineFactory.createEngineDescription(
-                JsonPropBankAnnotator.class, typeSystemDescription,
-                JsonPropBankAnnotator.PARAM_INPUT_COMPONENT_NAME, "allennlp",
-                JsonPropBankAnnotator.PARAM_JSON_SRL_INPUT_DIR, srlInput
+                AllenNLPJsonAnnotator.class, typeSystemDescription,
+                AllenNLPJsonAnnotator.PARAM_INPUT_COMPONENT_NAME, ALLENNLP_COMPONENT,
+                AllenNLPJsonAnnotator.PARAM_JSON_SRL_INPUT_DIR, srlInput
         );
 
         new BasicPipeline(reader, outputBase, "srl", annotator).run();
